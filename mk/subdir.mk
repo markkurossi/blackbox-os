4340345e8c2f797b7c89ac87d2ae4d7a19402572
Markku Rossi <mtr@iki.fi>
2018-09-05 07:44:51 +0300

    Make `vet' target.

--- mk/subdir.mk
+++ mk/subdir.mk
@@ -1,7 +1,10 @@
 GO1.11 := $(HOME)/work/go/bin/go1.11rc2
-ophony_targets = build
+phony_targets = build vet
 
 .PHONY: $(phony_targets)
 
 build:
 	GOOS=js GOARCH=wasm $(GO1.11) build
+
+vet:
+	GOOS=js GOARCH=wasm $(GO1.11) vet
