b5460a56b4cc712adde533cb8e9465e1cbf056e9
Markku Rossi <mtr@iki.fi>
2021-02-15 09:24:52 +0200

    Darwin stubs.

--- lib/bbos/syscall_darwin.go
+++ lib/bbos/syscall_darwin.go
@@ -10,8 +10,15 @@ import (
 	"fmt"
 )
 
+func JSByteArray(data []byte) []byte {
+	return data
+}
+
 func Syscall(call string, params map[string]interface{}) (
 	map[string]interface{}, error) {
 
 	return nil, fmt.Errorf("Syscall not implemented")
+}
+
+func SyscallSetWD(cwd string) {
 }
